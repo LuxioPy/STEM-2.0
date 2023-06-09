e8c442ab5b994814956b457d413b4e2ecf7e39e9
Zion Ceus <zionceus@gmail.com>
2023-06-09 04:24:43 -0400

    initial commit

---
+++ js/app.js
@@ -0,0 +1,19 @@
+//Slider
+var swiper = new Swiper(".hero__slider-thumbs", {
+      loop: true,
+      spaceBetween: 10,
+      slidesPerView: 4,
+      freeMode: true,
+      watchSlidesProgress: true,
+    });
+    var swiper2 = new Swiper(".hero__slider", {
+      loop: true,
+      spaceBetween: 10,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      thumbs: {
+        swiper: swiper,
+      },
+    });
